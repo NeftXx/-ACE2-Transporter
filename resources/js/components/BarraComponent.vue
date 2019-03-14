589fa549474a037f6896bcb4c66e285650a9967a
Neftxx <Ronald_201504420@outlook.com>
2019-03-14 10:58:47 -0600

    Se termino de configurar la base de datos

---
+++ resources/js/components/BarraComponent.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="barra">
+    <h1>Bar chart</h1>
+    <bar-chart
+        id="bar"
+        :data="barData"
+        :bar-colors="arrayColors" 
+        xkeys="year"
+        :ykeys="ykeys"
+        grid="true"
+        resize="true"
+        grid-text-weight="bold">
+    </bar-chart>
+</div>
+</template>
+
+<script>
+import { BarChart } from 'vue-morris'
+
+export default {
+    data(){
+        return {
+            barData: [
+                { year: '2012', car: 40 , motorcycle:300, airplane:60},
+                { year: '2013', car: 150, motorcycle:280, airplane:70},
+                { year: '2014', car: 100, motorcycle:150, airplane:30},
+                { year: '2015', car: 100, motorcycle:390, airplane:90}
+            ],
+            
+            xkeys: "year",
+
+            arrayColors: ["#5859f9", "#73c000", "#cc45ff"],
+      
+            ykeys: ["car", "motorcycle", "airplane"]
+        }
+  },
+  
+  components: {
+      BarChart
+  }
+}
+</script>
+
---
+++ resources/js/components/DonutComponent.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="donut">
+    <h1>Donut Chart</h1>
+    <donut-chart
+        id="donut"
+        :data="donutData"
+        :colors="arrayColors"
+        resize="true">
+    </donut-chart>
+</div>
+</template>
+
+<script>
+import { DonutChart } from 'vue-morris'
+
+export default {
+    data() {
+        return {
+            donutData: [
+                { label: 'Car', value: 40 },
+                { label: 'Motorcycle', value: 150 },
+                { label: 'airplane', value: 100 }
+            ],
+            arrayColors: [ "#FF6384", "#73c000", "#FFCE56" ]
+        }
+    },
+    components: {
+        DonutChart
+    }
+}
+</script>
+
+
+
