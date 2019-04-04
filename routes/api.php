6d14fdab512e2b6cfc58daf37b1c81ae62568404
Neftxx <Ronald_201504420@outlook.com>
2019-04-03 18:13:02 -0600

    Fixed: Cors

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,8 @@
 <?php
 
-Route::middleware('cors')->group(function() {
-
-        Route::get('bitacora', 'BitacoraController@index')
-        ->name('bitacora_get');
-
-        Route::get('nuevo-recorrido', 'BitacoraController@store');
-
-        Route::get('editar-recorrido', 'BitacoraController@edit');
-
-        Route::get('terminar-recorrido', 'BitacoraController@terminarRecorrido');
-
-        Route::get('recorrido-actual', 'BitacoraController@getLast');
-
-        Route::get('promedios', 'BitacoraController@obtenerPromedios');
-
-});
+Route::get('bitacora', 'BitacoraController@index')->name('bitacora_get');
+Route::get('nuevo-recorrido', 'BitacoraController@store');
+Route::get('editar-recorrido', 'BitacoraController@edit');
+Route::get('terminar-recorrido', 'BitacoraController@terminarRecorrido');
+Route::get('recorrido-actual', 'BitacoraController@getLast');
+Route::get('promedios', 'BitacoraController@obtenerPromedios');
