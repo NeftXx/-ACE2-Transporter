c519778b3c0b4b711d5ab0ba57be6ac8b34efee4
Neftxx <Ronald_201504420@outlook.com>
2019-04-03 19:59:11 -0600

    Primer demo

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,13 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import Raphael from 'raphael/raphael'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 global.Raphael = Raphael
 Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.component('donut-component', require('./components/DonutComponent.vue').default);
 Vue.component('barra-component', require('./components/BarraComponent.vue').default);
@@ -11,6 +16,7 @@ Vue.component('line-component', require('./components/LineComponent.vue').defaul
 Vue.component('navbar-component', require('./components/NavBarComponent.vue').default);
 Vue.component('card-component', require('./components/CardComponent.vue').default);
 Vue.component('dash-board-component', require('./components/DashBoardComponent.vue').default);
+Vue.component('recorridos-component', require('./components/TablaRecorridosComponent.vue').default);
 
 const app = new Vue({
     el: '#app'
--- resources/js/components/DashBoardComponent.vue
+++ resources/js/components/DashBoardComponent.vue
@@ -1,87 +1,154 @@
 <template>
-<div class="container" style="padding-top: 10px">
+  <div class="container" style="padding-top: 10px">
+
     <div class="col-lg-12 text-center">
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="mb-0 text-gray-800">Dashboard</h1>
+      <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="mb-0 text-gray-800">Dashboard</h1>
+      </div>
+    </div>
+
+    <div class="row">
+
+      <div class="col-4">
+
+        <h3>PROMEDIOS</h3>
+
+        <card-component
+          border="border-left-primary"
+          atributo="Velocidad"
+          :valor="info.velocidad_promedio"
+          icono="fas fa-bicycle"
+        />
+
+        <card-component
+          border="border-left-success"
+          atributo="Distancia"
+          :valor="info.distancia_promedio"
+          icono="fas fa-road"
+        />
+
+        <card-component
+          border="border-left-info"
+          atributo="Peso"
+          :valor="info.peso_promedio"
+          icono="fas fa-cube"
+        />
+
+        <card-component
+          border="border-left-warning"
+          atributo="Cantidad de obstaculos"
+          :valor="info.obstaculos_promedio"
+          icono="fas fa-cubes"
+        />
+
+        <card-component
+          border="border-left-danger"
+          atributo="Tiempo de espera"
+          :valor="info.espera_promedio"
+          icono="far fa-stop-circle"
+        />
+
+        <card-component
+          border="border-left-success"
+          atributo="Tiempo de ida"
+          :valor="info.inicio_promedio"
+          icono="far fa-clock"
+        />
+        <card-component
+          border="border-left-primary"
+          atributo="Tiempo de regreso"
+          :valor="info.fin_promedio"
+          icono="far fa-clock"
+        />
+
+      </div>
+
+      <div class="col-8">
+
+        <div class="card shadow mb-4">
+          <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+          </div>
+          <div class="card-body">
+            <div>
             </div>
+            <hr>
+            <code></code>
+          </div>
         </div>
-    <div class="row">                
-        <div class="col-4">
-            <h3> PROMEDIOS </h3>
-            <card-component border="border-left-primary"
-                    atributo="Velocidad"
-                    :valor="info.velocidad_promedio"
-                    icono="fas fa-bicycle" />
-            <card-component border="border-left-success"
-                    atributo="Distancia"
-                    :valor="info.distancia_promedio"
-                    icono="fas fa-road" />
-            <card-component border="border-left-info"
-                    atributo="Peso"
-                    :valor="info.peso_promedio"
-                    icono="fas fa-cube" />
-            <card-component border="border-left-warning"
-                    atributo="Cantidad de obstaculos"
-                    :valor="info.obstaculos_promedio"
-                    icono="fas fa-cubes" />
-            <card-component border="border-left-danger"
-                    atributo="Tiempo de espera"
-                    :valor="info.espera_promedio"
-                    icono="far fa-stop-circle" />
-            <card-component border="border-left-success"
-                    atributo="Tiempo de ida"
-                    :valor="info.inicio_promedio"
-                    icono="far fa-clock" />
-            <card-component border="border-left-primary"
-                    atributo="Tiempo de regreso"
-                    :valor="info.fin_promedio"
-                    icono="far fa-clock" />
-        </div>        
-    </div>    
-</div>
+
+        <div class="card shadow mb-4">
+          <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+          </div>
+          <div class="card-body">
+            <div>
+            </div>
+            <hr>
+            <code></code>
+          </div>
+        </div>
+
+        <div class="card shadow mb-4">
+          <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Tiempo de inicio y fin</h6>
+          </div>
+          <div class="card-body">
+            <div>
+            </div>
+            <hr>
+            <code></code>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import CardComponent from './CardComponent'
+import CardComponent from "./CardComponent";
+import BarraComponent from "./BarraComponent";
+import LineComponent from "./BarraComponent";
 
 export default {
-    data () {
-        return {
-            info: {
-                velocidad_promedio: 0,
-                distancia_promedio: 0,
-                peso_promedio: 0,
-                obstaculos_promedio: 0,
-                espera_promedio: 0,
-                inicio_promedio: 0,
-                fin_promedio: 0,
-            }
+  data() {
+    return {
+      info: {
+        velocidad_promedio: 0,
+        distancia_promedio: 0,
+        peso_promedio: 0,
+        obstaculos_promedio: 0,
+        espera_promedio: 0,
+        inicio_promedio: 0,
+        fin_promedio: 0
+      }
+    };
+  },
+
+  mounted() {
+    axios
+      .get("/api/promedios")
+      .then(response => {
+        this.info = response.data[0];
+        console.log(this.info);
+      })
+      .catch(error => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
         }
-    },
+        console.log(error.config);
+      });
+  },
 
-    mounted () {
-        axios
-        .get('/api/promedios')
-        .then((response) => {
-            this.info = response.data[0]
-            console.log(this.info);
-        })
-        .catch((error) => {
-            if (error.response) {
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log('Error', error.message);
-            }
-            console.log(error.config);
-        });
-    },
-
-    components: {
-        CardComponent
-    }
-}
+  components: {
+    CardComponent
+  }
+};
 </script>
